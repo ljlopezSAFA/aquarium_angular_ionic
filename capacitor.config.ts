371fbb8a07baf3e13918b7ec1a8c4cfe693ab7cb
ljlopez <ljlopez@fundacionsafa.es>
2025-09-29 12:10:49 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aquarium',
+  webDir: 'www'
+};
+
+export default config;
